d08f663763ccffea6ed8d51a7fb5fbd2b0bda662
ri0629 <tr08792@gmail.com>
2024-01-22 18:48:45 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   devise_for :users
+  root to: "messages#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
